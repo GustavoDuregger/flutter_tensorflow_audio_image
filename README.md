145c5c8fe1243c29cd44edb5502c3fdbea656218
Gustavo Duregger <gustavo.duregger@gmail.com>
2022-03-01 14:33:52 -0300

    readme

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-# flutter_tensorflow_audio
+# Classificação de imagens e áudios no flutter utilizando TensorFlow Lite
+Artigo completo no Medium: 
 
-Sua Musica Talks
+## Aviso
+As configurações do TensorFlow foram feitas apenas para Android, portanto não irá funcionar num dispositivo iOS.
 
-## Getting Started
+## Para rodar
+1. Clone o repositório
+```bash
+git clone https://github.com/GustavoDuregger/flutter_tensorflow_audio_image.git
+```
+2. Baixe as dependências do projeto
+```bash
+flutter pub get
+```
+3. Rode o projeto
+Run main.dart na IDE
 
-This project is a starting point for a Flutter application.
+## Estrutura
 
-A few resources to get you started if this is your first Flutter project:
+|__ assets (arquivos TFLite gerados pelo teachable machine)
+    |__ audio 
+    |__ image
+    |__ ui
+|__ lib
+    |__ common (compartilhado entre as features)
+        |__ ui
+            |__ constants (textos, cores, estilos)
+            |__ widgets (widgets que são compartilhados entre as features)
+    |__ features
+        |__ home (pagina inicial)
+        |__ audio_mic (feature para classificação de áudio - Musica X Podcast)
+        |__ image_camera (feature para classificação de imagem - Banana X Maçã)
+    |__ main
+|__ pubspec.yaml
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Screenshots
--- lib/features/audio_mic/presentation/widgets/label_list_widget.dart
+++ lib/features/audio_mic/presentation/widgets/label_list_widget.dart
@@ -3,35 +3,38 @@ import 'package:flutter_tensorflow_audio/common/ui/widgets/card_detector_widget.
 
 // Se o a stream bater com o resultado ira mudar o estado do card
 Widget labelListWidget(List<String>? labelList, [String? result]) {
-  return Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: labelList!.map((labels) {
-            if (labels == result) {
-              return cardDetectorWidget(
-                  1,
-                  1,
-                  'Resultado',
-                  labels.toString() == "0 Background Noise"
-                      ? "Ruído 💨"
-                      : labels.toString() == "1 Music"
-                      ? "Música 🎵"
-                      : labels.toString() == "2 Speech"
-                      ? "Podcast 🎙️"
-                      : "Desconhecido");
-            } else {
-              return cardDetectorWidget(
-                  2,
-                  1,
-                  'Resultado',
-                  labels.toString() == "0 Background Noise"
-                      ? "Ruído 💨"
-                      : labels.toString() == "1 Music"
-                      ? "Música 🎵"
-                      : labels.toString() == "2 Speech"
-                      ? "Podcast 🎙️"
-                      : "Desconhecido");
-            }
-          }).toList()));
+  return Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: labelList!.map((labels) {
+              if (labels == result) {
+                return cardDetectorWidget(
+                    1,
+                    1,
+                    'Resultado',
+                    labels.toString() == "0 Background Noise"
+                        ? "Ruído 💨"
+                        : labels.toString() == "1 Music"
+                        ? "Música 🎵"
+                        : labels.toString() == "2 Speech"
+                        ? "Podcast 🎙️"
+                        : "Desconhecido");
+              } else {
+                return cardDetectorWidget(
+                    2,
+                    1,
+                    'Resultado',
+                    labels.toString() == "0 Background Noise"
+                        ? "Ruído 💨"
+                        : labels.toString() == "1 Music"
+                        ? "Música 🎵"
+                        : labels.toString() == "2 Speech"
+                        ? "Podcast 🎙️"
+                        : "Desconhecido");
+              }
+            }).toList())),
+  );
 }
